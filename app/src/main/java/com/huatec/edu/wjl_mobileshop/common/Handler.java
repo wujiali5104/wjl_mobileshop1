d93f8590d71e8f253873d4216a491aceb3fd02cb
wujiali5104 <809945183@qq.com>
2019-11-29 14:06:45 +0800

    first wjl

---
+++ app/src/main/java/com/huatec/edu/wjl_mobileshop/common/Handler.java
@@ -0,0 +1,4 @@
+package com.huatec.edu.wjl_mobileshop.common;
+
+public class Handler extends android.os.Handler {
+}
---
+++ app/src/main/java/com/huatec/edu/wjl_mobileshop/fragment/CartFragment.java
@@ -0,0 +1,11 @@
+package com.huatec.edu.wjl_mobileshop.fragment;
+
+import com.huatec.edu.wjl_mobileshop.R;
+import com.huatec.edu.wjl_mobileshop.common.BaseFragment;
+
+public class CartFragment extends BaseFragment {
+    @Override
+    public int getContentViewId() {
+        return R.layout.fragment_cart;
+    }
+}
